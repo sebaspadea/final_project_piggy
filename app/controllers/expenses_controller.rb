cbb2f1359ddc26e9b1c02075ee01122780b7162b
pablo malczewski <malczewskipablo@gmail.com>
2020-08-04 15:05:59 -0300

    expenses controller

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,2 +1,28 @@
 class ExpensesController < ApplicationController
+
+  def index
+    @expenses = Expense.all
+  end
+
+
+  def new
+    @expence = Experience.new
+  end
+
+  def create
+    @expense = Expense.find(params[:expense_id])
+    if @expense.save!
+      redirect_to user_path(current_user)
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+   def expense_params
+   params.require(:expense).permit(:category, :amount, :description)
+  end
+
 end
