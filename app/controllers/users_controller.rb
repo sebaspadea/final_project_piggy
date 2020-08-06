cec5709acdc16fa7afa78d5b787ea659ab1f2b97
pablo malczewski <malczewskipablo@gmail.com>
2020-08-06 14:17:36 -0300

    cambios en la view de expenses

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,34 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def show
-    @user = current_user
     @expenses = current_user.expenses
     @savings = current_user.savings
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :saving_percentage, :username)
+  end
+
+  def set_user
+    @user = current_user
   end
 end
